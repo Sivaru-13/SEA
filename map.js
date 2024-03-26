27dcf5623a9bd0b50beb600aee2f40e598bc4b8f
Sivaru-13 <86969624+Sivaru-13@users.noreply.github.com>
2024-03-26 16:38:11 +0530

    Add files via upload

---
+++ map.js
@@ -0,0 +1,7 @@
+// Map location script, map.js
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
